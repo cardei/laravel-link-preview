34e216af8c41d53db00f1a8e7fda3361c260c39a
M. Catalin Cardei <mccardei@Ms-MacBook-Pro-2.local>
2024-10-02 09:23:40 +0200

    HD/29

--- src/Parsers/YouTubeParser.php
+++ src/Parsers/YouTubeParser.php
@@ -29,7 +29,7 @@ class YouTubeParser extends BaseParser implements ParserInterface
         $this->setPreview($preview ?: new VideoPreview());
 
         if (config('link-preview.enable_logging') && config('app.debug')) {
-            Log::debug('========================================== v2 HD 87 ==========================================');
+            Log::debug('========================================== v2 HD 29 ==========================================');
             Log::debug('🤩 YouTube Parser Initialized.'); 
         }
     }
@@ -145,9 +145,15 @@ class YouTubeParser extends BaseParser implements ParserInterface
                 Log::debug('-> Before updating preview object with YouTube API data:');
 
                 // Verificar y establecer el título, descripción y cover si están disponibles
-                $this->getPreview()->setTitle($snippet['title'] ?? 'No title available');
-                $this->getPreview()->setDescription($snippet['description'] ?? 'No description available');
-                $this->getPreview()->setCover($snippet['thumbnails']['high']['url'] ?? '');
+
+                $this->getPreview()->setId($videoId)
+                ->setEmbed(
+                    '<iframe id="ytplayer" type="text/html" width="640" height="390" src="' . e('//www.youtube.com/embed/'.$this->getPreview()->getId()) . '" frameborder="0"></iframe>'
+                );
+
+                // $this->getPreview()->setTitle($snippet['title'] ?? 'No title available');
+                // $this->getPreview()->setDescription($snippet['description'] ?? 'No description available');
+                // $this->getPreview()->setCover($snippet['thumbnails']['high']['url'] ?? '');
 
                 Log::debug('-> YouTube API Data updated in preview object with data:');
 
